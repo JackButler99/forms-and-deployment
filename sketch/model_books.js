cdf2e339b2392c5f0239c3eaf1117ad312f78c65
JackButler99 <ridhokurniawan32@gmail.com>
2021-11-23 13:44:16 +0700

    add the whole code

---
+++ sketch/model_books.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let BookSchema = newSchema({
+  title: {type: String, required: true},
+  author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+  summary: {type: String, required: true},
+  isbn: {type: String, required: true},
+  genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+})
